21d8798c377a7cb6d3c37e2f2d412f25f0149255
mickwickey@gmail.com <Mick@10.7.245.28>
2016-07-23 13:51:29 +0200

    second comit

--- TC_Test_1.py
+++ TC_Test_1.py
@@ -2,4 +2,4 @@ from selenium import webdriver
 
 driver = webdriver.Chrome('C:\\Users\\Mick\\Desktop\\Seleniump\\chromedriver')  # Optional argument, if not specified will search path.
 driver.get('http://www.google.com/xhtml')
-time.sleep(5) # Let the user actually see something!
+time.sleep(5) # Let the user actually see something!ff
