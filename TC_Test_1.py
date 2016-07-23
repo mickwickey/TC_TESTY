b83504d8a6f55b137a5f6cca8715fbb5734a4553
mickwickey@gmail.com <Mick@10.7.245.28>
2016-07-23 13:11:22 +0200

    First commit

---
+++ TC_Test_1.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome('C:\\Users\\Mick\\Desktop\\Seleniump\\chromedriver')  # Optional argument, if not specified will search path.
+driver.get('http://www.google.com/xhtml')
+time.sleep(5) # Let the user actually see something!
